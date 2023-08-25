f50c3cc1cc3a2b127d64ce501f51a8c3d1263de5
Valery <astrium@astrium.com>
2023-08-25 22:38:29 +0700

    Добавлена функция oct

--- first/nums.py
+++ first/nums.py
@@ -14,8 +14,13 @@ def dev(a, b):
     return a / b
 
 
+def oct(a, b):
+    return a % b
+
+
 if __name__ == '__main__':
     print(plus(2, 2))
     print(minus(4, 2))
     print(mult(3, 3))
     print(dev(100, 4))
+    print(oct(15, 4))
